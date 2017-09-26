8f36084372c415a829a36b776fc1457017e09d81
Fabien Veron <fabien.veron@gmail.com>
2017-09-26 23:44:36 +0800

    initial commit

---
+++ src/main/java/com/fabien/distrocache/DistrocacheApplication.java
@@ -0,0 +1,143 @@
+package com.fabien.distrocache;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+//@SpringBootApplication
+public class DistrocacheApplication {
+
+    public static void main(String[] args) {
+//		SpringApplication.run(DistrocacheApplication.class, args);
+
+        Node lisa = new Node("lisa");
+        Node bart = new Node("bart");
+        System.out.println("lisa = " + lisa);
+        System.out.println("bart = " + bart);
+        lisa.registerOtherNode(bart);
+        bart.registerOtherNode(lisa);
+
+        bart.setCacheAndReplicate("secret", 42);
+        System.out.println("secret = " + lisa.getCache("secret"));
+
+        IntStream.range(0,50).forEach( (n) -> {
+            new Thread(() -> {
+                lisa.setCacheAndReplicate("secret", 42);
+                System.out.println(bart.toString() + bart.getCache("secret"));
+            }).start();
+            new Thread(() -> {
+                bart.setCacheAndReplicate("secret", 78);
+                System.out.println(lisa.toString() + lisa.getCache("secret"));
+            }).start();
+        });
+    }
+}
+
+class Node {
+
+    String name;
+
+    boolean enabled;
+
+    List<Node> otherNodes;
+
+    HashMap<String, Integer> map;
+
+    public Node(String name) {
+        enabled = true;
+        map = new HashMap<>();
+        otherNodes = new ArrayList<>();
+
+        this.name = name;
+    }
+
+    public void registerOtherNode(Node otherNode) {
+        otherNodes.add(otherNode);
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    private void networkDelay() {
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setCacheAndReplicate(String key, int value) {
+        setCache(key,value);
+
+        for (Node otherNode : otherNodes) {
+            networkDelay();
+            otherNode.setCache(key,value);
+        }
+    }
+
+    public void setCache(String key, int value) {
+        if (!enabled)
+            return;
+
+        map.put(key, value);
+    }
+
+    public int getCache(String key) {
+        if (!enabled)
+            throw new IllegalStateException();
+
+        return map.get(key);
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "name='" + name + '\'' +
+                ", enabled=" + enabled +
+                '}';
+    }
+}
+
+class NodeService {
+    List<HashMap<String, Integer>> maps;
+
+    @PostConstruct
+    public void initCache() {
+        maps = new ArrayList<>();
+        IntStream.range(0, 2).forEach(i -> maps.add(new HashMap<>()));
+    }
+
+    public void setCache(int node, String key, int value) {
+        for (Map<String, Integer> map : maps) {
+            map.put(key, value);
+        }
+    }
+
+    public int getCache(int node, String key) {
+        return maps.get(node).get(key);
+    }
+}
+
+@RestController
+@RequestMapping("api/node/")
+class NodeController {
+
+    @RequestMapping
+    public String health() {
+        return "OK";
+    }
+
+    @RequestMapping("/set/{key}")
+    public void setCache(@PathVariable String key, @RequestBody String value) {
+
+    }
+}
---
+++ src/test/java/com/fabien/distrocache/DistrocacheApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fabien.distrocache;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DistrocacheApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
